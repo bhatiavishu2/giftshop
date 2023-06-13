9fedef20b800c928170db559cb7538158a6a4890
Vishav Rattan <vishav1.rattan@paytm.com>
2023-06-13 13:45:08 +0530

    transaction delete

---
+++ src/pages/orders.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useContext, useState } from "react";
+import Table from "components/core/Table"
+import {  useQuery,useMutation } from '@apollo/client';
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import {
+  getOrders
+} from "graphql/order";
+import { CommonStateContext } from "contexts/common";
+
+const Permissions = () => {
+  const { searchKeyword } = useContext(CommonStateContext);
+  const history = useHistory();
+  const { loading:isLoading, error, data = {}, refetch } = useQuery(getOrders);
+  const params = new Proxy(new URLSearchParams(history.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  useEffect(()=>{refetch()},[])
+  // const [deleteItem] = useMutation(deletePermission);
+  const [previewData, setPreviewData] = useState(null)
+  const [modalActive, setModalActive] = useState(false)
+  const { products} = data ;
+  let productsList =
+    products &&
+    products.filter((product) => {
+      return (
+        product.name.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+        !searchKeyword
+      );
+    });
+
+    if(params.productId){
+      productsList = productsList && productsList.filter(product => product.id === params.productId)
+    }
+
+
+
+
+  const handleOnDelete = async (data) =>{
+    // await deleteItem({ variables:
+    //   {id:data.id}
+    // })
+    await refetch()
+  }
+  const handleOnEdit = (data) => {
+    // history.push(`/editPermission/${data.id}`);
+  }
+
+  if (isLoading) {
+    return (
+      <div className="products-wrapper">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="container">
+       
+        <Table onEdit={handleOnEdit} data={data.orders} onDelete={handleOnDelete}/>
+        
+      </div>
+  );
+};
+
+export default Permissions;
